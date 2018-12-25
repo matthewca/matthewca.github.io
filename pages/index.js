a828e6f70721c6f6da697561b2cc47a2b43702ad
Matt Smith <matt@matt.ca>
2018-12-25 09:53:16 -0800

    add next.js

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+
+const Index = () => (
+    <div>
+        <Head>
+            <title>Matt Smith</title>
+            <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        </Head>
+        <p>Matt Smith</p>
+    </div>
+)
+
+export default Index;
