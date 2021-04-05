2c4b842d1d799e1db031fb7690672e028ad90573
matthewca <matt@matt.ca>
2021-04-05 08:08:48 -0700

    add yandex verification

--- _next/static/runtime/webpack.js
+++ _next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "1548c17c379b5e6c4f10";
+/******/ 	var hotCurrentHash = "1a3734d4d4048a4da878";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
